b71e0e36425c938c6aee73762645000e66446697
VBaraku <vbaraku@citycollege.sheffield.eu>
2022-03-18 17:39:42 +0200

    automated testing done for factory contract

--- test/factory.test.js
+++ test/factory.test.js
@@ -8,36 +8,32 @@ describe("Factory", function () {
     accounts = await ethers.getSigners();
   });
 
-  it("Should deply new contracts", async function () {
-      //deploy factory
-    const contractFactory = await deploy();
-    //have factory create 2 contracts
-    await factory.createPurchase(ethers.utils.parseEther("10"), {
-        msg.sender = accounts[0];
-        //not sure how to pass a different msg sender
-    });
-    await factory.createPurchase(ethers.utils.parseEther("11"), {
-        msg.sender = accounts[1];
-    });
+  it("Should deploy contract correctly", async function () {
+    const Factory = await ethers.getContractFactory("ContractFactory");
+    const contractFactory = await Factory.deploy();
 
-    //expect that when i call factory.getContracts() ill get the 2 contracts
-    await expect()(
-    );
+    await contractFactory
+      .connect(accounts[0])
+      .createPurchase(ethers.utils.parseEther("10"));
+    await contractFactory
+      .connect(accounts[1])
+      .createPurchase(ethers.utils.parseEther("11"));
+
+    const returnedAddresses = await contractFactory.getContracts();
+
+    const Purchase = await ethers.getContractFactory("Purchase");
+    let purchase = Purchase.attach(returnedAddresses[0]);
+    let purchase2 = Purchase.attach(returnedAddresses[1]);
+
+    await expect(await purchase.getOwner()).to.be.equal(accounts[0].address);
+    await expect(
+      await ethers.utils.formatEther(await purchase.getPrice())
+    ).to.be.equal("10.0");
+
+    await expect(await purchase2.getOwner()).to.be.equal(accounts[1].address);
+    await expect(
+      await ethers.utils.formatEther(await purchase2.getPrice())
+    ).to.be.equal("11.0");
   });
 
-  let factory;
-  beforeEach(async () => {
-    factory = await Factory.deployed();
-  });
-
-  it("Should not be able to buy own product", async function () {
-    const purchase = await deploy(
-      ethers.utils.parseEther("10"),
-      accounts[0].address
-    );
-
-    await expect(purchase.buy()).to.be.revertedWith(
-      "You are the seller you can't purchase your own product"
-    );
-  });
 });
