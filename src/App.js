0a2e9249747bb71fa6a09309037f40ce024e2266
VBaraku <vbaraku@citycollege.sheffield.eu>
2022-03-24 23:00:17 +0200

    Implemented all functions of sprint 1 with factory

--- src/App.js
+++ src/App.js
@@ -13,15 +13,10 @@ import Typography from "@mui/material/Typography";
 import AddProduct from "./components/AddProduct";
 
 const factoryAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
-const purchaseAddress = "0x5fbdb2315678afecb367f032d93f642f64180aa3";
 function App() {
   const [contracts, setContracts] = useState([]);
-  const [contractsData, setContractsData]  = useState([]);
-  const [price, setPrice] = useState();
+  const [contractsData, setContractsData] = useState([]);
   const [priceToChange, setPriceToChange] = useState();
-  const [owner, setOwner] = useState();
-
-  
 
   async function requestAccount() {
     await window.ethereum.request({ method: "eth_requestAccounts" });
@@ -36,33 +31,30 @@ function App() {
           ContractFactory.abi,
           provider
         );
-
         try {
           const data = await factory.getContracts();
-
           console.log(data);
           if (data != null) {
             let tempArray = [];
             data.forEach((e) => {
               const purchase = new ethers.Contract(e, Purchase.abi, provider);
               tempArray.push(purchase);
-              //let price1 = await purchase.getPrice();
             });
             setContracts(tempArray);
-          }
+            const promises = tempArray.map(async (e) => {
+              let price1 = ethers.utils.formatEther(await e.getPrice());
+              let owner1 = await e.getOwner();
+              let address1 = e.address;
+              let data = { price: price1, owner: owner1, address: address1 };
+              console.log(data);
+              return data;
+            });
 
-          const promises = contracts.map(async (e) => {
-            
-            let price1 = ethers.utils.formatEther(await e.getPrice());
-            let owner1 = await e.getOwner();
-            let data = {price : price1, owner: owner1};
-            return data;
-      
-          });
-          
-          Promise.all(promises).then(results => {
-            setContractsData(results);
-          })
+            Promise.all(promises).then((results) => {
+              console.log(results);
+              setContractsData(results);
+            });
+          }
         } catch (err) {
           console.log("error ", err);
         }
@@ -71,43 +63,32 @@ function App() {
     fetchData();
   }, []);
 
-  console.log(contracts);
-  console.log(contractsData);
-
-  async function setPriceOnContract() {
+  async function setPriceOnContract(address) {
     if (!contracts) return;
     if (typeof window.ethereum !== "undefined") {
       await requestAccount();
       const provider = new ethers.providers.Web3Provider(window.ethereum);
       const signer = provider.getSigner();
-      const contract = new ethers.Contract(
-        purchaseAddress,
-        Purchase.abi,
-        signer
-      );
+      const contract = new ethers.Contract(address, Purchase.abi, signer);
       try {
         const transaction = await contract.setPrice(
           ethers.utils.parseEther(priceToChange)
         );
         await transaction.wait();
-        setPrice(priceToChange);
       } catch (error) {
         alert("Only the owner can change the price");
         console.log(error);
       }
     }
+    window.location.reload(); // NEEDS TO BE CHANGED LATER TO UPDATE WITHOUT RELOADING
   }
 
-  async function purchaseProduct() {
+  async function purchaseProduct(address, price) {
     if (typeof window.ethereum !== "undefined") {
       await requestAccount();
       const provider = new ethers.providers.Web3Provider(window.ethereum);
       const signer = provider.getSigner();
-      const contract = new ethers.Contract(
-        purchaseAddress,
-        Purchase.abi,
-        signer
-      );
+      const contract = new ethers.Contract(address, Purchase.abi, signer);
       try {
         const transaction = await contract.buy({
           value: ethers.utils.parseEther(price),
@@ -120,59 +101,67 @@ function App() {
     }
   }
 
-  if (contracts != null) {
-
+  if (!contractsData.length) {
+    return "Loading";
+  } else {
     return (
       <div className="App">
         <header className="App-header">
-          <Card sx={{ minWidth: 275, maxWidth: 280 }}>
-            <CardContent>
-              <Typography
-                sx={{ fontSize: 14 }}
-                color="text.secondary"
-                gutterBottom
-              >
-                This is a smart contract
-              </Typography>
-              <Typography variant="h5" component="div">
-                Owner:
-              </Typography>
-              <Typography noWrap={true} sx={{ mb: 1.5 }} color="text.secondary">
-                {owner}
-              </Typography>
-              <Typography variant="body2">
-                The price of this contract is:
-                <br />
-                {price} eth
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button size="small" onClick={setPriceOnContract}>
-                Change price
-              </Button>
-              <input
-                size="4"
-                onChange={(e) => setPriceToChange(e.target.value)}
-                placeholder="set price"
-              ></input>
-              <Button
-                variant="contained"
-                position=""
-                color="success"
-                onClick={purchaseProduct}
-              >
-                Buy
-              </Button>
-            </CardActions>
-          </Card>
-
+          <div>
+            {contractsData.map((el) => (
+              <Card sx={{ minWidth: 275, maxWidth: 280, margin: 2 }}>
+                <CardContent>
+                  <Typography
+                    sx={{ fontSize: 14 }}
+                    color="text.secondary"
+                    gutterBottom
+                  >
+                    This is a smart contract
+                  </Typography>
+                  <Typography variant="h5" component="div">
+                    Owner:
+                  </Typography>
+                  <Typography
+                    noWrap={true}
+                    sx={{ mb: 1.5 }}
+                    color="text.secondary"
+                  >
+                    {el.owner}
+                  </Typography>
+                  <Typography variant="body2">
+                    The price of this contract is:
+                    <br />
+                    {el.price} eth
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button
+                    size="small"
+                    onClick={(e) => setPriceOnContract(el.address)}
+                  >
+                    Change price
+                  </Button>
+                  <input
+                    size="4"
+                    onChange={(e) => setPriceToChange(e.target.value)}
+                    placeholder="set price"
+                  ></input>
+                  <Button
+                    variant="contained"
+                    position=""
+                    color="success"
+                    onClick={(e) => purchaseProduct(el.address, el.price)}
+                  >
+                    Buy
+                  </Button>
+                </CardActions>
+              </Card>
+            ))}
+          </div>
           <AddProduct></AddProduct>
         </header>
       </div>
     );
-  } else {
-    return <>"Loading"</>;
   }
 }
-
 export default App;
