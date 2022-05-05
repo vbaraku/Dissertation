08e86021271ce27290e913c025fd14a2598be391
VBaraku <vbaraku@citycollege.sheffield.eu>
2022-05-05 14:38:13 +0300

    test cases added

--- test/purchase.test.js
+++ test/purchase.test.js
@@ -93,16 +93,14 @@ describe("Purchase", function () {
       testCIDs
     );
     const testHash1 = ethers.utils.keccak256("0x1234");
-    const testHash2 = ethers.utils.keccak256 ("0x5678");
-    const testHash3 = ethers.utils.keccak256 ("0x9999");
-    const testSampleKeys = [testHash1, testHash2, testHash3]
+    const testHash2 = ethers.utils.keccak256("0x5678");
+    const testHash3 = ethers.utils.keccak256("0x9999");
+    const testSampleKeys = [testHash1, testHash2, testHash3];
     const owner = purchase.connect(accounts[0]);
-    
-    await owner.pickHashedSample(testSampleKeys)
-    const pickedSample = await owner.returnRandomHashPicked()
-    await expect(
-      pickedSample
-    ).to.be.equal(testHash1);
+
+    await owner.pickHashedSample(testSampleKeys);
+    const pickedSample = await owner.returnRandomHashPicked();
+    await expect(pickedSample).to.be.equal(testHash1);
   });
 
   it("Should match unhashed to the initial hash", async function () {
@@ -111,18 +109,72 @@ describe("Purchase", function () {
       accounts[0].address,
       testCIDs
     );
-    const testUnHashed = '0x1234';
+    const testUnHashed = "0x1234";
     const testHash1 = ethers.utils.keccak256(testUnHashed);
 
-    const testSampleKeys = [testHash1]
+    const testSampleKeys = [testHash1];
     const owner = purchase.connect(accounts[0]);
-    
-    await owner.pickHashedSample(testSampleKeys)
+
+    await owner.pickHashedSample(testSampleKeys);
     await owner.putUnhashedSample(testUnHashed);
     const unHashReturned = await owner.returnUnHashedSample();
 
+    await expect(testUnHashed).to.be.equal(unHashReturned);
+  });
+
+  it("Should be able to purchase", async function () {
+    const purchase = await deploy(
+      ethers.utils.parseEther("10"),
+      accounts[0].address,
+      testCIDs
+    );
+    const price = ethers.utils.parseEther("10");
+    const buyer = purchase.connect(accounts[1]);
+    const buyerInitialBalance = await ethers.provider.getBalance(
+      accounts[1].address
+    );
+    const transaction = await buyer.purchaseProducts({
+      value: price,
+    });
+    const receipt = await transaction.wait();
+    const gasUsed = receipt.cumulativeGasUsed.mul(receipt.effectiveGasPrice);
     await expect(
-      testUnHashed
-    ).to.be.equal(unHashReturned);
+      await ethers.provider.getBalance(accounts[1].address)
+    ).to.be.equal(buyerInitialBalance.sub(price).sub(gasUsed));
+  });
+
+  it("Should be able to withdraw", async function () {
+    const purchase = await deploy(
+      ethers.utils.parseEther("20"),
+      accounts[0].address,
+      testCIDs
+    );
+    const owner = purchase.connect(accounts[0]);
+    const ownerInitialBalance = await ethers.provider.getBalance(
+      accounts[0].address
+    );
+    const buyer = purchase.connect(accounts[1]);
+    const price = ethers.utils.parseEther("20");
+
+    const testHash1 = ethers.utils.keccak256("0x1234");
+    const testHash2 = ethers.utils.keccak256("0x5678");
+    const testHash3 = ethers.utils.keccak256("0x9999");
+    const testSampleKeys = [testHash1, testHash2, testHash3];
+    const testUnHashedKeys = ["0x1234", "0x5678", "0x9999"];
+
+    const transaction1 = await owner.pickHashedSample(testSampleKeys);
+    const receipt1 = await transaction1.wait();
+    const gasUsed1 = receipt1.cumulativeGasUsed.mul(receipt1.effectiveGasPrice);
+    await buyer.purchaseProducts({
+      value: price,
+    });
+
+    const transaction2 = await owner.withdraw(testUnHashedKeys);
+    const receipt2 = await transaction2.wait();
+    const gasUsed2 = receipt2.cumulativeGasUsed.mul(receipt2.effectiveGasPrice);
+
+    await expect(
+      await ethers.provider.getBalance(accounts[0].address)
+    ).to.be.equal(ownerInitialBalance.add(price).sub(gasUsed1).sub(gasUsed2));
   });
 });
